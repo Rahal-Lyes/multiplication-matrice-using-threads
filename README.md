790728b84d8d6063573cda5702d615a11c6711f9
Rahal-Lyes <lyes.rahal@univ-bouira.dz>
2023-12-24 09:50:51 +0100

    Projet System Exploitation-lyesRahal

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# DSA
-Data Structure And Algorithm
+
+# <font color="red"> Projet **Systeme D'exploitation** Les Threads </font>
+
+##Q1: Quelles sont les structures de données à utiliser ?
+
+Il faut utiliser des structures de données pour représenter les matrices B, C, et A, 
+ainsi qu'un tampon T pour stocker les résultats intermédiaires. 
+On peut utiliser des tableaux bidimensionnels pour les matrices et un tableau unidimensionnel pour le tampon.
+
+##Q2: Comment allez-vous protéger l'accès à ces données?
+
+Pour protéger l'accès aux données partagées entre les threads, 
+on peut utiliser des verrous (mutex) et des sémaphores.
+ Un mutex peut être utilisé pour assurer l'accès exclusif aux matrices B, C, et A, 
+et un ensemble de sémaphores pour synchroniser la communication entre les producteurs et les consommateurs.
+
+##Q3: Quels sont les risques ?
+
+Les risques principaux incluent les conditions de concurrence (race conditions), 
+où plusieurs threads tentent d'accéder ou de modifier les mêmes données simultanément.
+ Cela peut conduire à des résultats imprévisibles et des erreurs dans le calcul des matrices.
---
+++ projectSe.c
@@ -0,0 +1,163 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define Thread 10
+#define N 10
+
+int n1, m1, n2, m2;
+int tompon[N];
+int count = 0;
+
+pthread_mutex_t mutex;
+sem_t semFull, semEmpty;
+
+int B[N][N]; 
+int A[N][N]; 
+int C[N][N];
+
+void *producer(void *args)
+{
+    int row = *(int *)args;
+    int somme;
+
+    for (int col = 0; col < m2; col++)
+    {
+        
+        somme = 0;
+        for (int i = 0; i < m1; i++)
+        {
+            somme += B[row][i] * A[i][col];
+        }
+
+       
+        sem_wait(&semEmpty);
+        pthread_mutex_lock(&mutex);
+        tompon[count] = somme;
+        count++;
+        pthread_mutex_unlock(&mutex);
+        sem_post(&semFull);
+    }
+
+    free(args);
+    pthread_exit(NULL);
+}
+
+void *consumer(void *args)
+{
+    int col = *(int *)args;
+    int result;
+
+    for (int row = 0; row < n1; row++)
+    {
+      
+        sem_wait(&semFull);
+        pthread_mutex_lock(&mutex);
+        count--;
+        result = tompon[count];
+        pthread_mutex_unlock(&mutex);
+        sem_post(&semEmpty);
+
+     
+        C[row][col] = result;
+    }
+
+    free(args);
+    pthread_exit(NULL);
+}
+
+int main()
+{
+    
+    n1 = m1 = n2 = m2 = 4; 
+
+    
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < m1; j++)
+        {
+            B[i][j] = rand() % 100;
+        }
+    }
+
+    for (int i = 0; i < n2; i++)
+    {
+        for (int j = 0; j < m2; j++)
+        {
+            A[i][j] = rand() % 100; 
+        }
+    }
+
+    pthread_t producers[Thread], consumers[Thread];
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&semFull, 0, 0);
+    sem_init(&semEmpty, 0, N);
+
+   
+    printf("Original Matrix A:\n");
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < m2; j++)
+        {
+            printf("%d ", A[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Original Matrix B:\n");
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < m2; j++)
+        {
+            printf("%d ", B[i][j]);
+        }
+        printf("\n");
+    }
+
+  
+    for (int i = 0; i < n1; i++)
+    {
+        int *a = malloc(sizeof(int));
+        *a = i;
+        pthread_create(&producers[i], NULL, producer, (void *)a);
+    }
+
+
+    for (int i = 0; i < m2; i++)
+    {
+        int *a = malloc(sizeof(int));
+        *a = i;
+        pthread_create(&consumers[i], NULL, consumer, (void *)a);
+    }
+
+
+    for (int i = 0; i < n1; i++)
+    {
+        pthread_join(producers[i], NULL);
+    }
+
+
+    for (int i = 0; i < m2; i++)
+    {
+        pthread_join(consumers[i], NULL);
+    }
+
+
+    printf("Resulting Matrix C:\n");
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < m2; j++)
+        {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&semFull);
+    sem_destroy(&semEmpty);
+
+    return 0;
+}
+
